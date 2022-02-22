530d1852ebaaf391e7e63bc3ca9481774be57206
P AMRITHNATH <84299133+p-amrithnath@users.noreply.github.com>
2022-02-22 07:21:54 -0800

    Update seclarge.java

--- Cycle2/seclarge.java
+++ Cycle2/seclarge.java
@@ -1,40 +1,26 @@
-import java.util.*;
-class seclarge
-{
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("How many elements: ");
+import java .util.*;
+class Seclarge {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+		System.out.print("How many elements");
 		int n = sc.nextInt();
+		int sl=0;
 		int i;
-		System.out.print("Enter the elements : ");
+		System.out.println("Enter the elements : ");
 		int[] a = new int[50];
 		for(i=0;i<n;i++)
 		{
 			a[i] = sc.nextInt();
 		}
-		int large;
-		int sec ;
-		if(a[0]<a[1])
+		int large = a[0];
+		for(i=1;i<n;i++)
 		{
-			large = a[0];
-			sec = a[1];
-		}
-		else
-		{
-			large=a[1];
-			sec = a[0];
-		}
-		for(i=0;i<n;i++)
-		{
-			if(a[i]>large)
-			{
-				sec = large;
-				large = a[i];
+			if(a[i]>small)
+   			{
+   			    sl=large;
+	   			large = a[i];
 			}
-			else if (a[i] > sec) 
-			{sec = a[i];}
 		}
-		System.out.println("The second largest element is " +sec);
-	}
+		System.out.println(large);
+    }
 }
